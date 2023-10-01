432baf07eaea721119d424ec37b3b8093a0af39d
Jason H <jasonhartantoo14@gmail.com>
2023-10-01 11:20:02 +0700

    logging to a file

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -7,7 +7,18 @@ import (
 )
 
 func Init() {
-	logrus.SetOutput(os.Stdout)
+	// Create or open a log file for writing
+	// logFile, err := os.OpenFile("logfile.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// if err != nil {
+	// 	logrus.Fatalf("Failed to open log file: %v", err)
+	// }
 
+	// Set the log output to the file
+	logrus.SetOutput(os.Stdout) // change os.Stdout to logFile to write to logfile.log
+
+	// Set log level
 	logrus.SetLevel(logrus.DebugLevel)
+
+	// Optionally, you can also set the log formatter, e.g., JSONFormatter
+	// logrus.SetFormatter(&logrus.JSONFormatter{})
 }
